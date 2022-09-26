bf251ced2ecc67f07082cc462e879c98d4dc9c52
Guilherme David <guilhermedavidrk@icloud.com>
2022-09-26 19:54:49 -0300

    :art::art: Create Modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,20 @@
 import { HeaderContainer, HeaderContent, NewTranslationButton } from './styles'
 import logoDT from '../../assets/logoDT.svg'
+import * as Dialog from '@radix-ui/react-dialog'
+import { NewTransactionsModal } from '../NewTransactionsModal'
 
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={logoDT} alt="" />
-        <NewTranslationButton>Nova transação</NewTranslationButton>
+
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTranslationButton>Nova transação</NewTranslationButton>
+          </Dialog.Trigger>
+          <NewTransactionsModal />
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   )
